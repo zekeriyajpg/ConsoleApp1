4b3185ba8f4540b754b18005ef2a3b0d3b541631
ZEK <ZEK@DESKTOP-MQI8H1N>
2024-07-16 09:15:42 +0300

    Proje dosyası ekle.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,4 @@
+﻿
+Console.WriteLine("Zekeriya Palabıyık" +
+    " patikanın uzman kadrosu ve tecrübeli eğitmenleri sayesinde yazılım dünyasına hızlı bir şekilde giriş yapabileceğim için bu bootcampdeyim");
+Console.ReadKey();
